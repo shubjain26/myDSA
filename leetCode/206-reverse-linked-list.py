34b88f0b74cab60a249d394fc4718eea6dedd720
shubjain26 <Shubjain26@gmail.com>
2022-10-09 20:40:39 +0530

    reversing a linked list, leetcode easy

---
+++ leetCode/206-reverse-linked-list.py
@@ -0,0 +1,36 @@
+"""
+Ref: https://leetcode.com/problems/reverse-linked-list/
+
+Difficulty: Easy
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Approach:
+    Traverse the linked list
+    Keep a prev_node as Null initially (this will be last node of reversed list).
+    For every node assign the prev_node as next node in reverse list.
+    prev_node is the final reversed list.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if head is None:
+            return None
+        cursor = head
+        prev_node = None
+        while True:
+            prev_node = ListNode(cursor.val, prev_node)
+            if not cursor.next is None:
+                cursor = cursor.next
+            else:
+                break
+        
+        return prev_node
